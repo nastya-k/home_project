f719aa7f8a06cb82bdc6468cad6c39e0c6ec5397
Anastasiia Kadurkina <kadurkinaanastasiia@yahoo.com>
2017-05-10 17:21:42 +0300

    Created layout and added plugins
    still need to make pixel perfect and use SASS

---
+++ assets/main.js
@@ -0,0 +1,78 @@
+$( document ).ready(function() {
+     $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+    
+});
+$('.slider-nav').slick({
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  infinite: true,
+  variableWidth: true,
+    lazyLoad: 'progressive',
+    draggable: true,
+    centerPadding: '0',
+    swipeToSlide: true
+});
+
+
+    
+//add a class when the document loads 
+    
+$('.slider-nav__item').each(function(){ 
+  if ($(this).hasClass('slick-current')){
+    $(this).children(".slider-nav__overlay").addClass("active__overlay");
+    $(this).children(".slider-nav__title").addClass("active__title");
+    $(this).children(".slider-nav__button").addClass("active__button"); 
+  }
+});
+ 
+ //slick-slider overlays after slides change  
+
+$('.slider-nav').on('afterChange', function(event, slick, currentSlide, nextSlide){ 
+$(".slider-nav__overlay").removeClass('active__overlay');
+$(".slider-nav__title").removeClass('active__title');
+$(".slider-nav__button").removeClass('active__button');
+$('.slick-current .slider-nav__overlay').addClass('active__overlay');
+$('.slick-current .slider-nav__title').addClass('active__title'); 
+$('.slick-current .slider-nav__button').addClass('active__button');  
+});    
+ 
+// masonry main
+
+    $('.mansory-wrapper').masonry({
+  itemSelector: '.grid-item',
+   columnWidth: '.grid-sizer',
+  percentPosition: true,
+    gutter: 30
+});
+    
+   // mansory         
+$('.grid-fancy__wrapper1').masonry({
+  itemSelector: '.grid-fancy__item',
+   columnWidth: '.grid-sizer2',
+   gutter: 3,
+  percentPosition: true
+
+});
+
+$('.grid-fancy__wrapper2').masonry({
+  itemSelector: '.fancy-item--small',
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+   gutter: 3
+}); 
+    
+// fancy-box
+ $("[data-fancybox]").fancybox({	
+});   
+
+
+ 
+});
